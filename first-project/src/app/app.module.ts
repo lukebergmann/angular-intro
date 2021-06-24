ff5710f9f182cca646b9b4ff354016d07cfe1ed7
lukebergmann <lukebergmann0@gmail.com>
2021-06-23 17:41:16 -0700

    Created a Server Component

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -4,15 +4,17 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
