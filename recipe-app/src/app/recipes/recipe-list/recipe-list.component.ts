772f057929f18fdfa33603cf751d963f00d994cf
lukebergmann <lukebergmann0@gmail.com>
2021-06-25 17:55:36 -0700

    The Basics section completed

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('World Famous Pizza', 'This is the best pizza ever!', 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg'),
-    new Recipe('Delicious Crispy Tofy', 'You will dream of this tofu!', 'https://d2wtgwi3o396m5.cloudfront.net/recipe/77f34ab7-7738-4c7e-b56a-4106941a15f6.jpg?d=1408x1120')
+    new Recipe('Delicious Crispy Tofu', 'You will dream of this tofu!', 'https://d2wtgwi3o396m5.cloudfront.net/recipe/77f34ab7-7738-4c7e-b56a-4106941a15f6.jpg?d=1408x1120')
   ];
   constructor() { }
 
