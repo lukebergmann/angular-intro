20bbc868d0fc182932951ac276e887b76cb0210c
lukebergmann <lukebergmann0@gmail.com>
2021-06-25 17:32:53 -0700

    added in recipes and have them dynamically added

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('World Famous Pizza', 'This is the best pizza ever!', 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg'),
+    new Recipe('Delicious Crispy Tofy', 'You will dream of this tofu!', 'https://d2wtgwi3o396m5.cloudfront.net/recipe/77f34ab7-7738-4c7e-b56a-4106941a15f6.jpg?d=1408x1120')
+  ];
   constructor() { }
 
   ngOnInit(): void {
