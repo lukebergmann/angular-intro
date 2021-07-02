656f963ae139cde7134d83329cd6293fc8bdaa3b
lukebergmann <lukebergmann0@gmail.com>
2021-07-02 14:47:50 -0700

    Added in delete functionality to backend server

--- Angular Practice/task-tracker/src/app/components/task-item/task-item.component.ts
+++ Angular Practice/task-tracker/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 @Component({
@@ -8,6 +8,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
 
   constructor() { }
@@ -15,4 +16,7 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDelete(task) {
+   this.onDeleteTask.emit(task);
+  }
 }
