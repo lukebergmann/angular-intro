5dcf87fc9406987b5ee5e3dde6c3572a6103afeb
lukebergmann <lukebergmann0@gmail.com>
2021-07-03 09:46:14 -0700

    created add-task toggle feature

--- Angular Practice/task-tracker/src/app/components/add-task/add-task.component.ts
+++ Angular Practice/task-tracker/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -11,7 +13,13 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
-  constructor() { }
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+
+  }
 
   ngOnInit(): void {
   }
@@ -27,7 +35,7 @@ export class AddTaskComponent implements OnInit {
       reminder: this.reminder
     };
 
-    this.onAddTask.emit(newTask)
+    this.onAddTask.emit(newTask);
 
     this.text = '';
     this.day = '';
